60fa57ebddc491c53ce1c6c28df07307e47cd54a
ian <ian.escarro@gmail.com>
2024-03-20 11:41:44 +0800

    add more util

---
+++ src/SharpIgnite/AppContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpIgnite
+{
+    public class AppContext
+    {
+        Input input;
+        Output output;
+        Database database;
+
+        private AppContext()
+        {
+            input = new Input();
+            output = new Output();
+
+            var databaseDriver = CreateDatabaseDriver();
+            database = new Database(databaseDriver);
+        }
+
+        static AppContext instance = new AppContext();
+
+        public IDatabaseDriver CreateDatabaseDriver()
+        {
+            return new SqlDatabaseDriver();
+        }
+
+        public static AppContext Instance {
+            get { return instance; }
+        }
+
+        public Database Database {
+            get { return database; }
+        }
+
+        public Input Input {
+            get { return input; }
+        }
+    }
+}
---
+++ src/SharpIgnite/Input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+
+namespace SharpIgnite
+{
+    public class Input
+    {
+        public string Post(string key)
+        {
+            return HttpContext.Current.Request.Form[key];
+        }
+        
+        public string Get(string key)
+        {
+            return HttpContext.Current.Request.QueryString[key];
+        }
+    }
+    
+    public class Output
+    {
+        bool profiler;
+        
+        public void EnableProfiler(bool profiler)
+        {
+            this.profiler = profiler;
+        }
+    }
+}
