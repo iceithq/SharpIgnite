3baf8ddfa437f144dc9d2ec1d3bbf0b843abed85
ian <ian.escarro@gmail.com>
2024-03-20 11:40:49 +0800

    init config

---
+++ src/SharpIgnite/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SharpIgnite
+{
+    public static class Config
+    {
+        public static string Item(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
