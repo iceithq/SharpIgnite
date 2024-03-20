204edb577f1614b954cb48f9e81e2a9b809c0579
ian <ian.escarro@gmail.com>
2024-03-20 11:41:35 +0800

    init web form

---
+++ src/SharpIgnite/WebForm.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SharpIgnite
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        public Database Database {
+            get { return AppContext.Instance.Database; }
+        }
+
+        public Input Input {
+            get { return AppContext.Instance.Input; }
+        }
+
+        public void Write(string s)
+        {
+            Response.Write(s);
+        }
+    }
+}
