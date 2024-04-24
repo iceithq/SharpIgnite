f619ad27cd40906c071e2bd4026c3505d8e9e68c
ian <ian.escarro@gmail.com>
2024-04-24 09:28:25 +0800

    add log4net

---
+++ src/SharpIgnite.Tests/LoggingHelperTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SharpIgnite.Tests
+{
+    [TestFixture]
+    public class LoggingHelperTests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            LoggingHelper.Info("This is info");
+            LoggingHelper.Debug("This is debug");
+            LoggingHelper.Error("This is error");
+        }
+    }
+}
